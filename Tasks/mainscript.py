a541ef0ad73d75fb6e79757d09b08a037620984e
Ian-Goodall-Halliwell <thinclab@queensu.ca>
2025-10-26 18:28:51 -0400

    changes

--- Tasks/mainscript.py
+++ Tasks/mainscript.py
@@ -77,7 +77,7 @@ class taskbattery(metadatacollection):
                 taskbattery.ESQtask = ESQtask
                 self.INFO = INFO
                 self.taskexeclist = []
-                self.win = visual.Window(size=(1440, 960),color='white',fullscr=False)
+                self.win = visual.Window(size=(1440, 960),color='white',fullscr=True)
                 self.text = text_2 = visual.TextStim(win=self.win, name='text_2',
                         text='Welcome to our experiment. \n Please follow the instructions on-screen and notify the attending researcher if anything is unclear \n We are thankful for your participation. \n Press <return/enter> to continue.',
                         font='Arial',
@@ -466,8 +466,6 @@ if __name__ == "__main__":
         event.waitKeys(keyList=['return'])
         tbt.win.flip()
 
-        #eventSegmentation.run_practice(tbt.win) #to run the practice before the battery
-
         # === PreFA ===
         print("Starting Free Association Task (before movies)...")
         freeAssoc.show_instructions(tbt.win)
@@ -485,6 +483,7 @@ if __name__ == "__main__":
                 VideoName=first_video_name
         )
 
+        eventSegmentation.run_practice(tbt.win) #to run the practice before the battery
         # Run movies
         tbt.run_battery()
 
